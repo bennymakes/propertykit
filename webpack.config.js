3a9b2646cb1b534d89f93a5f05b4008408f54159
Benjamin Broad <ben@broaditservices.com.au>
2019-05-21 20:57:37 +0930

    Added Babel & SCSS to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 var path = require("path");
 var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var extractPlugin = new ExtractTextPlugin({
+  filename: "main.css"
+});
 
 module.exports = {
   entry: "./src/js/app.js",
@@ -13,10 +18,23 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ["css-loader", "sass-loader"]
+        })
       }
     ]
   },
-  plugins: [
-    //..
-  ]
+  plugins: [extractPlugin]
 };
